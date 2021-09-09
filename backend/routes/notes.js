a031c207ef3fcefebedeb03cc14a54dfc892a573
shivamdattapurkayastha99 <67202581+shivamdattapurkayastha99@users.noreply.github.com>
2021-09-09 16:30:33 +0530

    second commit

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -36,4 +36,33 @@ router.post('/addnote', fetchUser, [
 
 
 })
+router.put('/updatenote/:id', fetchUser, [
+    
+
+
+], async (req, res) => {
+    const {title,desc,tag}=req.body
+    const newNote={};
+    if (title) {
+        newNote.title=title
+
+    }
+    if (desc) {
+        newNote.desc=desc
+
+    }
+    if (tag) {
+        newNote.tag=tag
+
+    }
+    const note=Notes.findById(req.params.id)
+    if (!note) {
+        return res.status(404).send("not found")
+    }
+    if (note.user.toString()!==req.user.id) {
+        return res.status(401).send("not allowed")
+    }
+    note=await Notes.findByIdAndUpdate(req.params.id,{$set:newNote},{new:true})
+    res.json({note})
+});
 module.exports = router
